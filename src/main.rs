28ea77eed8498bc4aa1666bca12ef93b583f3f8f
Robert812218 <robert812@shaw.ca>
2023-02-16 10:13:11 -0700

    fixed cargo.toml and main function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+#![no_std] 
+#![no_main] 
 
 use core::panic::PanicInfo;
 
@@ -8,8 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] 
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as * mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
-
