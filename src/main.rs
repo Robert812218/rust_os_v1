c0278c1cd502ed68294aab9b287748e4afba50ba
Robert812218 <robert812@shaw.ca>
2023-02-17 11:51:10 -0700

    add write string/new line/clear row methods

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-#![no_std] 
-#![no_main] 
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
+
 mod vga_buffer;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle] 
-pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    // let vga_buffer = 0xb8000 as * mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //    unsafe {
-    //        *vga_buffer.offset(i as isize * 2) = byte;
-    //        *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //    }
-    // }
-
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
