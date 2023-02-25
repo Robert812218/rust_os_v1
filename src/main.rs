a07e3294508ae8c871eb2a9f6379edfd5369fb8b
Robert812218 <robert812@shaw.ca>
2023-02-25 10:48:13 -0700

    add task state segment

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ pub extern "C" fn _start() -> ! {
 
     rust_os_v1::init();
 
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow();
+    }
 
     #[cfg(test)]
     test_main();
