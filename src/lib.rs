ff2df92b47ad1eee6e761dd555822586ec33abe5
Robert812218 <robert812@shaw.ca>
2023-02-26 11:04:48 -0700

    add timer interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,10 @@ where
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub fn test_runner(tests: &[&dyn Testable]) {
