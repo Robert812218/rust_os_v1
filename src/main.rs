2d8d900a8dbe58061a33996518e73d61e273972a
Robert812218 <robert812@shaw.ca>
2023-03-01 13:37:57 -0700

    update table entries

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 use rust_os_v1::println;
 use core::panic::PanicInfo;
 use bootloader::{entry_point, BootInfo};
+use x86_64::paging::PageTable;
 
 entry_point!(kernel_main);
 
@@ -59,4 +60,19 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     println!("It did not crash!");
     rust_os_v1::hlt_loop();
+}
+
+if !entry.is_unused() {
+    println!("L4 Entry {}: {:?}", i, entry);
+
+    let phys = entry.frame().unwrap().start_address();
+    let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    let ptr = VirtAddr::new(virt).as_mut_ptr();
+    let l3_table: &PageTable = unsafe { &*ptr };
+
+    for (i, entry) in 13_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("  L3 Entry {}: {:?}", i, entry);
+        }
+    }
 }
