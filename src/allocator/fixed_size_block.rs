d1d348a1c630a82e2aa15c36f506b78fb6e8ad55
Robert812218 <robert812@shaw.ca>
2023-03-09 13:56:26 -0700

    add fixed size block

---
+++ src/allocator/fixed_size_block.rs
@@ -0,0 +1,31 @@
+use alloc::alloc::Layout;
+use core::ptr;
+
+struct ListNode {
+    next: Option<&'static mut ListNode>,
+}
+
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+
+impl FixedSizeBlockAllocator {
+    pub const fn new() -> Self {
+        const EMPTY: Option<&'static mut ListNode> = None;
+        FixedSizeBlockAllocator {
+            list_heads: [EMPTY; BLOCK_SIZES.len()],
+            fallback_allocator: linked_list_allocator::Heap::empty(),
+        }
+    }
+
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.fallback_allocator.init(heap_start, heap_size);
+    }
+
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
+    }
+}
+
+
